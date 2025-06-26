a253a64c5c502c63c663e3804d480136f915413b
Lenkly2 <lenklygbs10@gmail.com>
2025-06-26 19:53:09 +0300

    rdz1

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+
+from .models import Post
 
 # Create your views here.
+
+def post_list(request:HttpRequest):
+    posts = Post.objects.all()
+    print(posts)
+    return render(request,"blogapp/post_list.html", context={"list":posts})
