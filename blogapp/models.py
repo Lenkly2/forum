a01746f14aa51a59603839c476953b995e953e7c
Lenkly2 <lenklygbs10@gmail.com>
2025-06-26 20:37:26 +0300

    test3

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
@@ -7,3 +7,22 @@ class Post(models.Model):
     context = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     edited_date = models.DateTimeField(auto_now_add=True)
+
+    def published_recently(self):
+        today = timezone.now()
+        # if today - self.published_date < 7:
+        #     return "True"
+        a = today - self.published_date
+        return a
+        
+    def __str__(self):
+        return self.title
+    
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+    bio = models.TextField()
+
+    author = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
--- blogapp/views.py
+++ blogapp/views.py
@@ -8,4 +8,6 @@ from .models import Post
 def post_list(request:HttpRequest):
     posts = Post.objects.all()
     print(posts)
+    post1 = posts[0]
+    print(post1.published_recently())
     return render(request,"blogapp/post_list.html", context={"list":posts})
